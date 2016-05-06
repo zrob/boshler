5ab27342e1bab78ed4348617343f4e00b092a48a
Zach Robinson <zrobinson@pivotal.io>
2016-05-06 12:07:33 -0700

    add bosh cli upload release

---
+++ bosh_cli/bosh_cli.go
@@ -0,0 +1,17 @@
+package bosh_cli
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func UploadRelease(releasePath string) error {
+	fmt.Printf("Uploading release %s...\n", releasePath)
+	cmd := exec.Command("bosh", "upload", "release", releasePath)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Done uploading release %s.\n", releasePath)
+	return nil
+}
